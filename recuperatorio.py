94622f0d98f9af7f639306d130e071c67d99242f
enedamian <142589183+enedamian@users.noreply.github.com>
2023-10-20 09:36:39 -0300

    acutalizo consigna

--- recuperatorio.py
+++ recuperatorio.py
@@ -1,15 +1,18 @@
+# Recuperatorio Primer Parcial - Programación 2 - TUP UTN
+
 nombre=''
 fechaNacimiento=''
 dni=''
 legajo=''
 
-# 1) implementar una funcion hashRecuperatorio(stringNombre, stringFecha) que recibe un nombre y una fecha de nacimiento y devuelve un numero 
-# entero que representa el hash de la persona. stringFecha debe tener el formato "dd-mm-aaaa". 
+# 1) implementar una funcion hashRecuperatorio(stringNombre, stringFecha) que recibe un nombre y una fecha de nacimiento 
+# y devuelve un numero entero que representa el hash de la persona. stringFecha debe tener el formato "dd-mm-aaaa". 
 # El hash se calculará de la siguiente forma:
 # Se deben sumar los valores ASCII de los caracteres del nombre. (los valores ASCII se obtienen con la funcion "ord(caracter)" de python).
-# Luego, si la suma es par se le suma el DNI, sino se le suma el Legajo. 
+# Luego, si la suma anterior es par se le debe sumar el DNI, sino se le suma el Legajo. 
 # Finalmente la función debe retornar el residuo de la división por 3 del resultado anterior. (módulo de la división por 3)
 # Recordá validar los datos para que el programa no termine con errores.
+# Sólo es necesario programar la función, no el ingreso de los datos.
 
 def hashRecuperatorio(stringNombre, stringFecha):
 
@@ -49,8 +52,13 @@ def procesarVentas(compradores, cantidadPaquetesDisponibles):
     
     return 0
 
-#5) Implementar una función que reciba un diccionario con las claves como nombres de personas y los valores como edades y devuelva un diccionario con las claves como edades y los valores una LISTA con los nombres de las personas de esa edad.
-dicEdades = {"Juan":30,"María":25,"Carlos":35,"Ana":28,"Luis":32,"Laura":29,"Pedro":31,"Sofía":26,"Miguel":27,"Isabel":24,"Diego":33,"Valentina":22,"Javier":29,"Carmen":40,"Andrés":38,"Lucía":27,"Ricardo":36,"Victoria":23,"Gabriel":34,"Fernanda":31,"Max":29,"Diana":28,"Rodrigo":30,"Camila":25,"Gonzalo":33,"Daniela":27,"Emilio":29,"Marcela":34,"José":39,"Valeria":26,"Andrea":30,"Raúl":42,"Sara":24,"Rafael":36,"Elena":25,"Martín":32,"Beatriz":28,"Pablo":37,"Lorena":30,"Ángel":29,"Paula":26,"Luisa":35,"Arturo":31,"Mónica":27,"Manuel":28,"Verónica":29,"Carlos":40}
+#5) Implementar una función que reciba un diccionario con las claves como nombres de personas y los valores como edades 
+# y devuelva un diccionario con las claves como edades y los valores una LISTA con los nombres de las personas de esa edad.
+dicEdades = {"Juan":30,"María":25,"Carlos":35,"Ana":28,"Luis":32,"Laura":29,"Pedro":31,"Sofía":26,"Miguel":27,"Isabel":24,
+             "Diego":33,"Valentina":22,"Javier":29,"Carmen":40,"Andrés":38,"Lucía":27,"Ricardo":36,"Victoria":23,"Gabriel":34,
+             "Fernanda":31,"Max":29,"Diana":28,"Rodrigo":30,"Camila":25,"Gonzalo":33,"Daniela":27,"Emilio":29,"Marcela":34,
+             "José":39,"Valeria":26,"Andrea":30,"Raúl":42,"Sara":24,"Rafael":36,"Elena":25,"Martín":32,"Beatriz":28,"Pablo":37,
+             "Lorena":30,"Ángel":29,"Paula":26,"Luisa":35,"Arturo":31,"Mónica":27,"Manuel":28,"Verónica":29,"Carlos":40}
 # Resultado esperado
 #personas_por_edad = {
 #    22: ["Valentina"],
